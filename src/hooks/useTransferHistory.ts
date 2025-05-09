7d3f37f8f64437a838bed50e05fab8bfd3097ec9
manufac-shehla <shehla@manufacanalytics.com>
2025-05-09 13:35:33 +0530

    minor

--- src/hooks/useTransferHistory.ts
+++ src/hooks/useTransferHistory.ts
@@ -12,22 +12,17 @@ export function useTransferHistory(address: string) {
   return useQuery<AssetTransfersResponse>({
     queryKey: ['transferHistory', address],
     queryFn: async () => {
-      try {
-        const transfersData = await AlchemyInstance.core.getAssetTransfers({
-          fromAddress: address,
-          category: [
-            AssetTransfersCategory.EXTERNAL,
-            AssetTransfersCategory.INTERNAL,
-            AssetTransfersCategory.ERC20,
-            AssetTransfersCategory.ERC721,
-            AssetTransfersCategory.ERC1155,
-          ],
-        });
-        return transfersData; 
-      } catch (error) {
-        console.error('Error fetching transfer history:', error);
-        throw new Error('Failed to fetch transfer history');
-      }
+      const transfersData = await AlchemyInstance.core.getAssetTransfers({
+        fromAddress: address,
+        category: [
+          AssetTransfersCategory.EXTERNAL,
+          AssetTransfersCategory.INTERNAL,
+          AssetTransfersCategory.ERC20,
+          AssetTransfersCategory.ERC721,
+          AssetTransfersCategory.ERC1155,
+        ],
+      });
+      return transfersData;
     },
     enabled: typeof address === 'string' && address.trim().length > 0,
     refetchOnWindowFocus: false,
