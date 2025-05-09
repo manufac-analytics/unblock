8fac30345454a4b408c7026d8d85eea107161782
manufac-shehla <shehla@manufacanalytics.com>
2025-05-09 14:32:07 +0530

    minor

--- src/hooks/useTransferHistory.ts
+++ src/hooks/useTransferHistory.ts
@@ -3,20 +3,23 @@ import { useQuery } from "@tanstack/react-query";
 import { AssetTransfersCategory } from "alchemy-sdk";
 import type { AssetTransfersResponse } from "alchemy-sdk";
 
-export function useTransferHistory(address: string) {
+export function useTransferHistory(
+  address: string,
+  categories: AssetTransfersCategory[] = [
+    AssetTransfersCategory.EXTERNAL,
+    AssetTransfersCategory.INTERNAL,
+    AssetTransfersCategory.ERC20,
+    AssetTransfersCategory.ERC721,
+    AssetTransfersCategory.ERC1155,
+    AssetTransfersCategory.SPECIALNFT,
+  ],
+) {
   return useQuery<AssetTransfersResponse>({
-    queryKey: ["transferHistory", address],
+    queryKey: ["transferHistory", address, categories],
     queryFn: async (): Promise<AssetTransfersResponse> => {
       const transfersData = await AlchemyInstance.core.getAssetTransfers({
         fromAddress: address,
-        category: [
-          AssetTransfersCategory.EXTERNAL,
-          AssetTransfersCategory.INTERNAL,
-          AssetTransfersCategory.ERC20,
-          AssetTransfersCategory.ERC721,
-          AssetTransfersCategory.ERC1155,
-          AssetTransfersCategory.SPECIALNFT,
-        ],
+        category: categories,
       });
 
       return transfersData;
