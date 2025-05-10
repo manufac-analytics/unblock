b58f4a44ae69c65e15f8485e338015931ae9cc51
manufac-shehla <shehla@manufacanalytics.com>
2025-05-10 13:01:10 +0530

    token balamce ui part four

--- src/pages/Page-A/index.tsx
+++ src/pages/Page-A/index.tsx
@@ -1,9 +1,36 @@
-import { Title } from "@mantine/core";
+import { TokenBalance } from "../../components/TokenBalance";
+import { useBalances } from "../../hooks/useBalances";
+import { Input, Stack, Loader, Button, Container, Group } from "@mantine/core";
+import { useState } from "react";
 
 export function PageA() {
+  const [address, setAddress] = useState("");
+  const [submittedAddress, setSubmittedAddress] = useState("");
+  const { data, isLoading: loading } = useBalances(submittedAddress);
+  const handleCheckBalance = () => {
+    setSubmittedAddress(address.trim());
+  };
+
   return (
-    <Title order={6} ta="center">
-      This is page A
-    </Title>
+    <Container fluid>
+      <Stack align="center">
+        <Group>
+          <Input
+            placeholder="Enter wallet address"
+            value={address}
+            onChange={(e) => {
+              setAddress(e.currentTarget.value);
+            }}
+          />
+          <Button onClick={handleCheckBalance} disabled={loading === true}>
+            {loading === true ? <Loader size="sm" /> : "Check Balance"}
+          </Button>
+        </Group>
+
+        {loading === false && data !== undefined && (
+          <TokenBalance ethBalance={data.ethBalance} tokenBalances={data.tokenBalances} />
+        )}
+      </Stack>
+    </Container>
   );
 }
