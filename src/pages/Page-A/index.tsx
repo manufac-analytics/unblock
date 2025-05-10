f2e0d48ad55ee0172d7cadcb0a5eb2e2526d027e
manufac-shehla <shehla@manufacanalytics.com>
2025-05-10 14:09:43 +0530

    historical ui transfer part four

--- src/pages/Page-A/index.tsx
+++ src/pages/Page-A/index.tsx
@@ -1,9 +1,34 @@
-import { Title } from "@mantine/core";
+import { HistoricalTransfer } from "../../components/HistoricalTransfer";
+import { useTransferHistory } from "../../hooks/useTransferHistory";
+import { Input, Stack, Loader, Button, Container, Group } from "@mantine/core";
+import { useState } from "react";
 
 export function PageA() {
+  const [inputAddress, setInputAddress] = useState("");
+  const [walletAddress, setWalletAddress] = useState("");
+  const { data, isLoading } = useTransferHistory(walletAddress);
+
+  const handleFetchTransfers = () => {
+    setWalletAddress(inputAddress.trim());
+  };
+
   return (
-    <Title order={6} ta="center">
-      This is page A
-    </Title>
+    <Container fluid>
+      <Stack align="center">
+        <Group>
+          <Input
+            placeholder="Enter wallet address"
+            value={inputAddress}
+            onChange={(e) => {
+              setInputAddress(e.currentTarget.value);
+            }}
+          />
+          <Button onClick={handleFetchTransfers} disabled={isLoading}>
+            {isLoading === true ? <Loader size="sm" /> : "Get Transfer History"}
+          </Button>
+        </Group>
+        {isLoading === false && data !== undefined && <HistoricalTransfer transfers={data} />}
+      </Stack>
+    </Container>
   );
 }
