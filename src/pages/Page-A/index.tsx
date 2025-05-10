90b17dd23387eb88029e585f7164b4869f6a1d38
manufac-shehla <shehla@manufacanalytics.com>
2025-05-10 14:30:36 +0530

    minor

--- src/pages/Page-A/index.tsx
+++ src/pages/Page-A/index.tsx
@@ -1,10 +1,11 @@
 import { TokenBalance } from "../../components/TokenBalance";
 import { useBalances } from "../../hooks/useBalances";
 import { Input, Stack, Button, Container, Group } from "@mantine/core";
+import { useInputState } from "@mantine/hooks";
 import { useState } from "react";
 
 export function PageA() {
-  const [address, setAddress] = useState("");
+  const [address, setAddress] = useInputState("");
   const [submittedAddress, setSubmittedAddress] = useState("");
   const { data, isLoading: loading } = useBalances(submittedAddress);
   const handleCheckBalance = () => {
@@ -15,21 +16,15 @@ export function PageA() {
     <Container fluid>
       <Stack align="center">
         <Group>
-          <Input
-            placeholder="Enter wallet address"
-            value={address}
-            onChange={(e) => {
-              setAddress(e.currentTarget.value);
-            }}
-          />
+          <Input placeholder="Enter wallet address" value={address} onChange={setAddress} />
           <Button onClick={handleCheckBalance} loading={loading}>
             Check Balance
           </Button>
         </Group>
 
-        {loading === false && data !== undefined && (
+        {loading === false && data !== undefined ? (
           <TokenBalance ethBalance={data.ethBalance} tokenBalances={data.tokenBalances} />
-        )}
+        ) : null}
       </Stack>
     </Container>
   );
